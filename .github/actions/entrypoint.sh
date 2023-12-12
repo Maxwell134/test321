7b9c70f947d89cc7d9de5121c215c2c62bb232b3
Maxwell134 <maxbasumatry@gmail.com>
2023-12-12 23:09:59 +0530

    changes made

--- .github/actions/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-# .github/actions/test-my-name/entrypoint.sh
-#!/bin/sh
-
-echo "::set-output name=greeting::Hello, $1!"
--- .github/actions/test-action/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-# .github/actions/test-action/entrypoint.sh
-#!/bin/sh
-
-echo "Hello, $1!"
