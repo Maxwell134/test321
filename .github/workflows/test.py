8ec9234544579dec8794e14b11038395eb1fbbf7
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-14 14:45:07 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import os
 
-def employees(employee_id, employee):
+def employee_exists(employee_id, employee):
     if employee in employee_id:
         current_time = datetime.now().strftime('%I:%M:%S %p')
         print('current_time:', current_time)
@@ -11,9 +11,9 @@ def employees(employee_id, employee):
     return 'Not found'
 
 def main():
-    employee = os.environ.get('emp_name')  # Fix the typo here: "get" instead of "get,"
+    employee = os.environ.get('emp_name')
     employee_id = ['001', '002', '003', '004', '005']
-    result = employees(employee_id, employee)
+    result = employee_exists(employee_id, employee)
     print(result)
 
 if __name__ == '__main__':
