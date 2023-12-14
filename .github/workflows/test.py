65e34cdae0c7d6639f45cfd480e1ed40fea6113b
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-14 17:43:03 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,21 +1,30 @@
 from datetime import datetime
 import os
 
-def employee_exists(employee_id, employee):
+def find_employee(employee_id, employee):
     if employee in employee_id:
         current_time = datetime.now().strftime('%I:%M:%S %p')
         print('current_time:', current_time)
         print(f'Employee: {employee} exists in the list')
         return 'Found'
-
     return 'Not found'
 
 def main():
-    employee = os.environ.get('employee')
-    employee_id = ['001', '002', '003', '004', '005']
-    result = employee_exists(employee_id, employee)
-    print(result)
-    print(f"::set-output name=result::{result}")
+    try:
+        prompt = os.environ.get('PROMPT', '').lower()
+        if prompt == 'yes':
+            employee_id = ['001', '002', '003', '004', '005']
+            employee_input = os.environ.get('employee', '')
+
+            result = find_employee(employee_id, employee_input)
+            print(result)
+        elif prompt == 'no':
+            print('Exiting')
+        else:
+            print('Invalid input.')
+    except Exception as e:
+        print(f'Error: {str(e)}')
+
 
 if __name__ == '__main__':
     main()
