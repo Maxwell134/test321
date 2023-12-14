141a945e3ae57885164c8fe715762a35ed8e1b0d
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-14 14:21:58 +0530

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,24 +1,20 @@
 from datetime import datetime
+import os
+
 def employees(employee_id, employee):
-
     if employee in employee_id:
-
         current_time = datetime.now().strftime('%I:%M:%S %p')
         print('current_time:', current_time)
-        print(f'Employee: {employee} exist in the list')
+        print(f'Employee: {employee} exists in the list')
         return 'Found'
 
-    return "Not found"
-
+    return 'Not found'
 
 def main():
-
-    employee = input('Enter the employee id: ')
+    employee = os.environ.get('emp_name')  # Fix the typo here: "get" instead of "get,"
     employee_id = ['001', '002', '003', '004', '005']
     result = employees(employee_id, employee)
     print(result)
 
-
 if __name__ == '__main__':
-
     main()
