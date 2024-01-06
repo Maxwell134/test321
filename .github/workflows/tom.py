47db0acfb57db96ae50f45e15b7fdd1a18b34d21
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-01-06 23:45:46 +0530

    Update tom.py

--- .github/workflows/tom.py
+++ .github/workflows/tom.py
@@ -1,12 +1,11 @@
-
 import requests
 import json
 import os
 
-token = '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'
+token = os.getenv('GITHUB_TOKEN')
 username = 'Maxwell134'
 Repositoryname = 'test321'
-number = 16
+
 """
 Step 1:
 Create contents for the issue you want to post
@@ -22,16 +21,13 @@ Step 2:
 Generate your target repository's URL using Github API
 # """
 
-url = "https://api.github.com/repos/{}/{}/issues/{}".format(username,Repositoryname, number)
+url = "https://api.github.com/repos/{}/{}/issues".format(username,Repositoryname)
 
 """
 Step 3:
 Post your issue message using requests and json
 """
-response = requests.post(url,data=json.dumps(data),headers=headers)
-
-print(response.text)
-
-print(f'https://api.github.com/repos/{username}/{Repositoryname}/issues/{number}')
+requests.post(url,data=json.dumps(data),headers=headers)
+print(f'https://github.com/{username}/{Repositoryname}/issues')
 
 
