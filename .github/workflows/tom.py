b41427a15e520aa74166fa70c3be72e7abc44a7a
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-01-08 00:02:40 +0530

    Update tom.py

--- .github/workflows/tom.py
+++ .github/workflows/tom.py
@@ -1,10 +1,12 @@
 import json
 import os
 import requests
+from random import randint
 
 token = os.getenv('GITHUB_TOKEN')
 username = os.environ.get('username')
 Repositoryname = os.environ.get('repo')
+issue_number = randint(1,20)
 
 """
 Step 1:
@@ -20,7 +22,7 @@ data = {
 Step 2:
 Generate your target repository's URL using Github API
 """
-url = f"https://api.github.com/repos/{username}/{Repositoryname}/issues"
+url = f"https://api.github.com/repos/{username}/{Repositoryname}/issues/{issue_number}"
 
 """
 Step 3:
@@ -28,6 +30,27 @@ Post your issue message using requests and json
 """
 requests.post(url, data=json.dumps(data), headers=headers)
 
-print(f'https://github.com/{username}/{Repositoryname}/issues')
+issue = f'https://github.com/{username}/{Repositoryname}/issues/{issue_number}'
+os.popen(f'echo {issue}')
 
+time.sleep(180)
+
+comment_url = f'https://api.github.com/repos/{username}/{Repositoryname}/issues/{issue_number}/comments'
+headers = {'Authorization': f'token {token}'}
+
+# Get comments on the issue
+comments = requests.get(comment_url, headers=headers)
+
+comment = any('yes' in comment['body']for comment in comments.json())
+
+print(comment)
+close_url = f'https://api.github.com/repos/{username}/{Repositoryname}/issues/{issue_number}'
+headers = {'Authorization': f'token {token}'}
+
+if comment:
+    response = requests.patch(close_url, headers=headers, json={'state': 'closed'})
+    print(response.json()['state'])
+
+else:
+    print('sorry')
 
