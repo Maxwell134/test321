818684bfa0ba47bb30b3be58cde3b1d04d4f988b
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-01-08 01:26:12 +0530

    Create issue.py

---
+++ .github/workflows/issue.py
@@ -0,0 +1,27 @@
+import json
+import os
+import requests
+
+
+token = os.getenv('GITHUB_TOKEN')
+username = os.environ.get('username')
+Repositoryname = os.environ.get('repo')
+
+comment_url = f'https://api.github.com/repos/{username}/{Repositoryname}/issues/{issue_number}/comments'
+headers = {'Authorization': f'token {token}'}
+
+# Get comments on the issue
+comments = requests.get(comment_url, headers=headers)
+
+comment = any('yes' in comment['body']for comment in comments.json())
+
+print(comment)
+close_url = f'https://api.github.com/repos/{username}/{Repositoryname}/issues/{issue_number}'
+headers = {'Authorization': f'token {token}'}
+
+if comment:
+    response = requests.patch(close_url, headers=headers, json={'state': 'closed'})
+    print(response.json()['state'])
+
+else:
+    print('sorry')
