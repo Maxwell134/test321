7fd56eb912e7461719d34c9b57c5314363a20c3a
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2024-01-07 19:53:16 +0530

    Create automata.py

---
+++ .github/workflows/automata.py
@@ -0,0 +1,47 @@
+
+import csv
+import random
+import time
+
+import requests
+
+def send_message():
+
+    token = os.getenv('credentials)
+    quotes = []
+    with open('Quotes.csv', 'r', newline='') as reader:
+        lines = csv.DictReader(reader)
+
+        for line in lines:
+           quotes.append(line['Quotes'])
+
+    message = random.choice(quotes)
+
+    url = os.getenv('url')
+    chatId = os.getenv('chatId')
+
+    payload = {
+        "chatId": chatId,
+        "message": message
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {token}"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    print(response.text)
+
+
+def main():
+  send_message()
+
+if __name__ == '__main__' :
+
+  main()
+
+
+
+
